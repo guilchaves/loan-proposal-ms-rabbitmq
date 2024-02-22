778a91b620c72cc76a21f5a743685b489c7f4dbb
guilherme chaves <gchaves.guilherme@gmail.com>
2024-02-22 16:25:37 -0300

    first commit

---
+++ credit-offer-app/src/main/java/br/com/guilchaves/creditofferapp/CreditOfferAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.guilchaves.creditofferapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditOfferAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditOfferAppApplication.class, args);
+	}
+
+}
---
+++ credit-offer-app/src/test/java/br/com/guilchaves/creditofferapp/CreditOfferAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guilchaves.creditofferapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditOfferAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
