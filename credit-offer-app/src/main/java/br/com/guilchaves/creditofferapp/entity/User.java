3042af04ec906066f6adc70cbd2830c93d00dd08
guilherme chaves <gchaves.guilherme@gmail.com>
2024-02-22 17:31:08 -0300

    feat(entity): add User entity

---
+++ credit-offer-app/src/main/java/br/com/guilchaves/creditofferapp/entity/User.java
@@ -0,0 +1,21 @@
+package br.com.guilchaves.creditofferapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String lastName;
+    private String document;
+    private String phone;
+    private Double income;
+
+    @OneToOne(mappedBy = "user")
+    private Proposal proposal;
+
+}
