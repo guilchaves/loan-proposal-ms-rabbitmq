9f2faa2af91506b57cb904c39fa55f17e679b753
guilherme chaves <gchaves.guilherme@gmail.com>
2024-02-23 16:13:47 -0300

    feat: add create queue methods to rabbitmq config

--- credit-offer-app/src/main/java/br/com/guilchaves/creditofferapp/config/RabbitMQConfiguration.java
+++ credit-offer-app/src/main/java/br/com/guilchaves/creditofferapp/config/RabbitMQConfiguration.java
@@ -1,6 +1,10 @@
 package br.com.guilchaves.creditofferapp.config;
 
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +15,36 @@ import org.springframework.amqp.core.Queue;
 public class RabbitMQConfiguration {
 
     @Bean
-    public Queue createQueue(){
-        return QueueBuilder.durable("").build();
+    public RabbitAdmin createRabbitAdmin(ConnectionFactory connectionFactory){
+        return new RabbitAdmin(connectionFactory);
     }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> initializeAdmin(RabbitAdmin rabbitAdmin){
+        return event -> rabbitAdmin.initialize();
+    }
+
+    //Todo
+    // extract create queue methods to corresponding microservices
+    @Bean
+    public Queue createQueueProposalPendingMsCreditAnalysis(){
+        return QueueBuilder.durable("proposal-pending.ms-credit-analysis").build();
+    }
+
+    @Bean
+    public Queue createQueueProposalPendingMsNotification(){
+        return QueueBuilder.durable("proposal-pending.ms-notification").build();
+    }
+
+    @Bean
+    public Queue createQueueProposalFinishedMsProposal(){
+        return QueueBuilder.durable("proposal-finished.ms-proposal").build();
+    }
+
+    @Bean
+    public Queue createQueueProposalFinishedMsNotification(){
+        return QueueBuilder.durable("proposal-finished.ms-notification").build();
+    }
+
 
 }
