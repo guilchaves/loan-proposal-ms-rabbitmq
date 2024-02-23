5f89022c3b0ec992774ac27323fb11e917ac5845
guilherme chaves <gchaves.guilherme@gmail.com>
2024-02-23 11:42:43 -0300

    refactor: rename package for consistency, add mapper impls

--- credit-offer-app/src/main/java/br/com/guilchaves/creditofferapp/mappers/ProposalMapper.java
+++ credit-offer-app/src/main/java/br/com/guilchaves/creditofferapp/mappers/ProposalMapper.java
@@ -1,12 +1,16 @@
-package br.com.guilchaves.creditofferapp.mapper;
+package br.com.guilchaves.creditofferapp.mappers;
 
 import br.com.guilchaves.creditofferapp.dto.ProposalRequestDTO;
+import br.com.guilchaves.creditofferapp.dto.ProposalResponseDTO;
 import br.com.guilchaves.creditofferapp.entities.Proposal;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface ProposalMapper {
+
+    ProposalMapper INSTANCE = Mappers.getMapper(ProposalMapper.class);
 
     @Mapping(target = "user.name", source = "name")
     @Mapping(target = "user.lastName", source = "lastName")
@@ -17,5 +21,13 @@ public interface ProposalMapper {
     @Mapping(target = "approved", ignore = true)
     @Mapping(target = "integrated", ignore = true)
     @Mapping(target = "observation", ignore = true)
-    Proposal convertDtoToProposal(ProposalRequestDTO proposalRequest);
+    Proposal convertDtoToEntity(ProposalRequestDTO dto);
+
+
+    @Mapping(target = "name", source = "user.name")
+    @Mapping(target = "lastName", source = "user.lastName")
+    @Mapping(target = "phone", source = "user.phone")
+    @Mapping(target = "document", source = "user.document")
+    @Mapping(target = "income", source = "user.income")
+    ProposalResponseDTO convertEntityToDto(Proposal entity);
 }
