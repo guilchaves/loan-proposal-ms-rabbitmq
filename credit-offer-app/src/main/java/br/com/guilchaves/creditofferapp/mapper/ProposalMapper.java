6d3d4cf7b22cb1cbe683613aa1e41347e9e23b97
guilherme chaves <gchaves.guilherme@gmail.com>
2024-02-22 21:45:50 -0300

    feat: add proposal mapper

--- credit-offer-app/src/main/java/br/com/guilchaves/creditofferapp/mapper/ProposalMapper.java
+++ credit-offer-app/src/main/java/br/com/guilchaves/creditofferapp/mapper/ProposalMapper.java
@@ -3,9 +3,19 @@ package br.com.guilchaves.creditofferapp.mapper;
 import br.com.guilchaves.creditofferapp.dto.ProposalRequestDTO;
 import br.com.guilchaves.creditofferapp.entities.Proposal;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface ProposalMapper {
 
+    @Mapping(target = "user.name", source = "name")
+    @Mapping(target = "user.lastName", source = "lastName")
+    @Mapping(target = "user.document", source = "document")
+    @Mapping(target = "user.phone", source = "phone")
+    @Mapping(target = "user.income", source = "income")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "approved", ignore = true)
+    @Mapping(target = "integrated", ignore = true)
+    @Mapping(target = "observation", ignore = true)
     Proposal convertDtoToProposal(ProposalRequestDTO proposalRequest);
 }
