f07fb8e7b530ba0ab2e13977341b34307c49525d
guilherme chaves <gchaves.guilherme@gmail.com>
2024-02-26 16:35:58 -0300

    feat: add proposal and user domain

---
+++ notification/src/main/java/br/com/guilchaves/notification/domain/Proposal.java
@@ -0,0 +1,71 @@
+package br.com.guilchaves.notification.domain;
+
+public class Proposal {
+
+    private Long id;
+    private Double requestedAmount;
+    private int loanRepaymentTermMonths;
+    private Boolean approved;
+    private boolean integrated;
+    private String observation;
+    private User user;
+
+    public Proposal() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getRequestedAmount() {
+        return requestedAmount;
+    }
+
+    public void setRequestedAmount(Double requestedAmount) {
+        this.requestedAmount = requestedAmount;
+    }
+
+    public int getLoanRepaymentTermMonths() {
+        return loanRepaymentTermMonths;
+    }
+
+    public void setLoanRepaymentTermMonths(int loanRepaymentTermMonths) {
+        this.loanRepaymentTermMonths = loanRepaymentTermMonths;
+    }
+
+    public Boolean getApproved() {
+        return approved;
+    }
+
+    public void setApproved(Boolean approved) {
+        this.approved = approved;
+    }
+
+    public boolean isIntegrated() {
+        return integrated;
+    }
+
+    public void setIntegrated(boolean integrated) {
+        this.integrated = integrated;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
