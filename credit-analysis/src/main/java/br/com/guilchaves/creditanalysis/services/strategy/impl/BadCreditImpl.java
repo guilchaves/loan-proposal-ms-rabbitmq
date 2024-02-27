8213d8f969a2d33e43d8b60a188ff01f3ca523ef
guilherme chaves <gchaves.guilherme@gmail.com>
2024-02-27 19:00:52 -0300

    feat: add order to impl

--- credit-analysis/src/main/java/br/com/guilchaves/creditanalysis/services/strategy/impl/BadCreditImpl.java
+++ credit-analysis/src/main/java/br/com/guilchaves/creditanalysis/services/strategy/impl/BadCreditImpl.java
@@ -2,10 +2,12 @@ package br.com.guilchaves.creditanalysis.services.strategy.impl;
 
 import br.com.guilchaves.creditanalysis.domain.Proposal;
 import br.com.guilchaves.creditanalysis.services.strategy.InternalScoreCalculation;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
+@Order(1)
 @Component
 public class BadCreditImpl implements InternalScoreCalculation {
 
