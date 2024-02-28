382f33dc087fd33e7aa8725a16c513d4244bcb05
guilherme chaves <gchaves.guilherme@gmail.com>
2024-02-28 18:13:46 -0300

    chore: implemented reviewProposal method

--- credit-analysis/src/main/java/br/com/guilchaves/creditanalysis/listeners/ProposalReviewListener.java
+++ credit-analysis/src/main/java/br/com/guilchaves/creditanalysis/listeners/ProposalReviewListener.java
@@ -1,14 +1,19 @@
 package br.com.guilchaves.creditanalysis.listeners;
 
 import br.com.guilchaves.creditanalysis.domain.Proposal;
+import br.com.guilchaves.creditanalysis.services.CreditAnalysisService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ProposalReviewListener {
 
+    @Autowired
+    private CreditAnalysisService creditAnalysisService;
+
     @RabbitListener(queues = "${rabbitmq.queue.proposal.pending}")
     public void reviewProposal(Proposal proposal){
-
+        creditAnalysisService.analyze(proposal);
     }
 }
