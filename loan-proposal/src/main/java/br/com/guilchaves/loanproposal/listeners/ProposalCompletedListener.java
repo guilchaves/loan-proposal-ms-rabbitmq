77294b1449f9ba78abaa11028ad331c68aa94205
guilherme chaves <gchaves.guilherme@gmail.com>
2024-02-28 19:28:50 -0300

    chore: implemented completed proposal listener

--- loan-proposal/src/main/java/br/com/guilchaves/loanproposal/listeners/ProposalCompletedListener.java
+++ loan-proposal/src/main/java/br/com/guilchaves/loanproposal/listeners/ProposalCompletedListener.java
@@ -1,7 +1,10 @@
 package br.com.guilchaves.loanproposal.listeners;
 
+import br.com.guilchaves.loanproposal.dto.ProposalResponseDTO;
 import br.com.guilchaves.loanproposal.entities.Proposal;
+import br.com.guilchaves.loanproposal.mappers.ProposalMapper;
 import br.com.guilchaves.loanproposal.repositories.ProposalRepository;
+import br.com.guilchaves.loanproposal.services.WebSocketService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,9 +15,14 @@ public class ProposalCompletedListener {
     @Autowired
     private ProposalRepository proposalRepository;
 
+    @Autowired
+    private WebSocketService webSocketService;
+
     @RabbitListener(queues = "${rabbitmq.queue.proposal.completed}")
     public void proposalCompleted(Proposal proposal){
         proposalRepository.save(proposal);
+        ProposalResponseDTO responseDTO = ProposalMapper.INSTANCE.convertEntityToDto(proposal);
+        webSocketService.sendNotification(responseDTO);
     }
 
 }
